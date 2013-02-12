f49e6a4b63d27dbb8c925d0c359cd12165e42c10
jdelic <jm@Parasite.(none)>
2013-02-12 14:00:36 +0100

    PEP8 whitespace fixes

--- djangopypi2/apps/pypi_frontend/distutils_request.py
+++ djangopypi2/apps/pypi_frontend/distutils_request.py
@@ -15,8 +15,10 @@ def _get_distutils_action(request):
         action = request.GET.get(':action', None)
     return action
 
+
 def is_distutils_request(request):
     return _get_distutils_action(request) is not None
+
 
 def handle_distutils_request(request):
     action = _get_distutils_action(request)
@@ -27,26 +29,29 @@ def handle_distutils_request(request):
 
     return ACTION_VIEWS[action](request)
 
+
 def _parse_header(header):
     headers = {}
     for kvpair in filter(lambda p: p,
                          map(lambda p: p.strip(),
                              header.split(';'))):
         try:
-            key, value = kvpair.split("=",1)
+            key, value = kvpair.split("=", 1)
         except ValueError:
             continue
         headers[key.strip()] = value.strip('"')
-    
+
     return headers
 
+
 def parse_distutils_request(request):
-    """ This is being used because the built in request parser that Django uses,
+    """
+    This is being used because the built in request parser that Django uses,
     django.http.multipartparser.MultiPartParser is interperting the POST data
     incorrectly and/or the post data coming from distutils is invalid.
-    
-    One portion of this is the end marker: \r\n\r\n (what Django expects) 
-    versus \n\n (what distutils is sending). 
+
+    One portion of this is the end marker: \r\n\r\n (what Django expects)
+    versus \n\n (what distutils is sending).
     """
     try:
         sep = request.raw_post_data.splitlines()[1]
@@ -58,24 +63,24 @@ def parse_distutils_request(request):
         request._files = MultiValueDict()
     except Exception, e:
         pass
-    
+
     for part in filter(lambda e: e.strip(), request.raw_post_data.split(sep)):
         try:
-            header, content = part.lstrip().split('\n',1)
+            header, content = part.lstrip().split('\n', 1)
         except Exception, e:
             continue
-        
+
         if content.startswith('\n'):
             content = content[1:]
-        
+
         if content.endswith('\n'):
             content = content[:-1]
-        
+
         headers = _parse_header(header)
-        
+
         if "name" not in headers:
             continue
-        
+
         if "filename" in headers:
             dist = TemporaryUploadedFile(name=headers["filename"],
                                          size=len(content),
